d22bd2a7afa23f075298f19de60554903784ce6f
Jonathan Day <jonathan@aligent.com.au>
2015-05-20 11:17:45 +0930

    custom attributes

--- importCategories.php
+++ importCategories.php
@@ -67,6 +67,13 @@ class Mage_Shell_ImportCategories extends Mage_Shell_Abstract
         $category->setMetaDescription($data[18]);
         $category->setThumbnail($data[19]);
         $category->setImage($data[20]);
+        $category->setHeaderBackgroundColour($data[21]);
+        $category->setHeaderTextBackgroundColour($data[22]);
+        $category->setHeaderTextDescription($data[23]);
+        $category->setHeaderSubHeading($data[24]);
+        $category->setHeaderHeadingColour($data[25]);
+        $category->setHeaderBodyColour($data[26]);
+        $category->setHeaderSubHeadingColour($data[27]);
         $category->save();
         $category = Mage::getModel('catalog/category')->load($data[0]);
         echo $category->getId() ? $category->getId() : '⚡';
